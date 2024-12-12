6bb324923d4f685e5d51debf73f436a88a93a1ab
Andrew Rothstein <andrew.rothstein@gmail.com>
2024-12-12 01:02:14 +0000

    loft:4.1.1

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -10,33 +10,30 @@ dl()
     local ver=$3
     local os=$4
     local arch=$5
-    local dotexe=${6:-}
-    local platform="${os}-${arch}"
-    # https://github.com/loft-sh/loft/releases/download/v3.3.4/loft-linux-amd64
-    local file="${app}-${platform}${dotexe}"
-    local url="${MIRROR}/${ver}/${file}"
-    printf "      # %s\n" $url
-    printf "      %s: sha256:%s\n" $platform $(grep -e "$file\$" $lchecksums | grep -v devspace | awk '{print $1}')
+    local archive_type=${6:-tar.gz}
+    local platform="${os}_${arch}"
+    # https://github.com/loft-sh/loft/releases/download/v4.1.1/loft_4.1.1_linux_amd64.tar.gz
+    local file="${app}_${ver}_${platform}.${archive_type}"
+    local url="${MIRROR}/v${ver}/${file}"
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(grep -e "$file\$" $lchecksums | awk '{print $1}')
 }
 
 dl_ver() {
     local ver=$1
-    local lchecksums="${DIR}/loft-${ver}-checksums.txt"
+    local lchecksums="${DIR}/loft-v${ver}-checksums.txt"
     # https://github.com/loft-sh/loft/releases/download/v3.3.4/checksums.txt
-    local url="${MIRROR}/${ver}/checksums.txt"
+    # https://github.com/loft-sh/loft/releases/download/v4.1.1/checksums.txt
+    local url="${MIRROR}/v${ver}/checksums.txt"
     if [ ! -e $lchecksums ];
     then
         curl -sSLf -o $lchecksums $url
     fi
 
     printf "  # %s\n" $url
-    printf "  %s:\n" $ver
-    printf "    %s:\n" loft
-    dl loft $lchecksums $ver darwin amd64
-    dl loft $lchecksums $ver darwin arm64
+    printf "  '%s':\n" $ver
     dl loft $lchecksums $ver linux amd64
     dl loft $lchecksums $ver linux arm64
-    dl loft $lchecksums $ver windows amd64 .exe
 }
 
-dl_ver ${1:-v3.4.9}
+dl_ver ${1:-4.1.1}
